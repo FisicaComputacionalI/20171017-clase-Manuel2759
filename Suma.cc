fa1c83538e73443cc68133deb1a3e672e977a119
Manuel2759 <31254539+Manuel2759@users.noreply.github.com>
2017-10-17 13:50:52 -0500

    Add files via upload

--- Suma.cc
+++ Suma.cc
@@ -1,19 +1,21 @@
-//Programa que calcula la suma de numeros impares del 1 al 10
+//Suma del 1 al 20 de los numeros pares (incluyendo el 20)
 //Juan Manuel Badillo Serrano
-//Martes 17 de Octubre de 2017
+//Martes 17 de octubre del 2017
 #include <iostream>
 using namespace std;
 int main()
 {
   int sum=0;
   int count=1;
-  while (count<10){
-    if (count%2>0)
+  while (count<=20)
+    { if (count%2 == 0)
       {
 	sum=sum+count;
       }
-    count++;
-  }
-  cout<<"sum "<<sum<<endl;
-  return 0;
+      count++;
 }
+ cout<<"suma = "<<sum<<endl;
+ return 0;
+}
+//(==) es para comparar
+//La suma es 110
